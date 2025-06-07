426bceab7917b28c8c543268f0cec2a9830eba26
Leticia-SS <leticia.saraiva013@gmail.com>
2025-06-07 16:03:18 -0300

    exercicio 3, criando classe City e chamando a lista em Country

--- CityBreaks/Models/Country.cs
+++ CityBreaks/Models/Country.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string CountryCode { get; set; }
         public string CountryName { get; set; }
+        public List<City> Cities { get; set; } = new List<City>();
 
     }
 }
