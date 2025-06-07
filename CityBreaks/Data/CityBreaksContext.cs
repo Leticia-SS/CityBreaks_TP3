ede75102e271777e8fce105c7a5ee8cbe696aeab
Leticia-SS <leticia.saraiva013@gmail.com>
2025-06-07 15:56:26 -0300

    Adicionar arquivos de projeto.

---
+++ CityBreaks/Data/CityBreaksContext.cs
@@ -0,0 +1,12 @@
+﻿using CityBreaks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityBreaks.Data
+{
+    public class CityBreaksContext : DbContext
+    {
+        DbSet<Country> Countries { get; set; }
+        public CityBreaksContext(DbContextOptions<CityBreaksContext> options) : base(options) { }
+    }
+}
+
